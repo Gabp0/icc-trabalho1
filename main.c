746b28ec480d348417b7ce8dc3a163ab6b0726a8
Casadororo <51424037+Casadororo@users.noreply.github.com>
2022-03-29 16:16:23 -0300

    teste commit

--- main.c
+++ main.c
@@ -6,4 +6,30 @@ int main(int argc, char **argv)
 {
     char *output = getArgs(argc, argv);
     printf("%s\n", output);
+
+    // cabeçalho
+    printf("Iteração \t| Newton Padrão \t| Newton Modificado \t| Newton Inexato\n");
+
+    // para cada iteração
+    for (...)
+    {
+        printf("%d \t\t| ", i); // imprime iteração
+
+        if (...)
+        { // se nesta iteração o valor da primeira coluna existe, imprime
+            if (isnan(fx) || isinf(fx))
+                printf("%1.14e\t\t\t| ", fx);
+            else
+                printf("%1.14e\t| ", fx);
+        }
+        else
+            printf("\t\t\t| ");
+
+        // repete para as outras duas colunas...
+    }
+
+    // imprimir os tempos
+    printf("Tempo total \t| %1.14e\t| %1.14e\t| %1.14e\n", TtotalEG, TtotalLU, TtotalGS);
+    printf("Tempo derivadas | %1.14e\t| %1.14e\t| %1.14e\n", TderivadasEG, TderivadasLU, TderivadasGS);
+    printf("Tempo SL \t| %1.14e\t| %1.14e\t| %1.14e\n", TslEG, TslLU, TslGS);
 }
